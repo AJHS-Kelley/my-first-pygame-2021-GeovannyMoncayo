261757c16339ec6f758afde3cc85abbe80acce86
Geovanny <s11575131@students.duvalschools.org>
2021-12-09 14:37:02 -0500

    v0.6
    Filled in our windowSurface.
    
    Adjust MOVESPEED of the boxes based on locations.

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Geovanny Moncayo, 12/09/21, 2:26PM, v0.5
+# Simple Animation with PyGame, Geovanny Moncayo, 12/09/21, 2:36PM, v0.6
 
 from _typeshed import ReadableBuffer
 import pygame, sys, time
@@ -40,3 +40,20 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    windowSurface.fill(WHITE)
+
+    for b in boxes:
+        # Move the box data structure.
+        if b['dir'] == DOWNLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b['dir'] == DOWNRIGHT:
+            b['rect'].left += MOVESPEED
+            b['rect'].top += MOVESPEED
+        if b['dir'] == UPLEFT:
+            b['rect'].left -= MOVESPEED
+            b['rect'].top -= MOVESPEED
+        if b['dir'] == UPRIGHT:
+            b['rect'] += MOVESPEED
+            b['rect'] -= MOVESPEED
